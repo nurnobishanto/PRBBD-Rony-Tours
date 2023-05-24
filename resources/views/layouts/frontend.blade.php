3f7979871e0c7c81f01e7507a15619623c8e61ee
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-05-24 13:43:47 +0600

    Update index.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -48,9 +48,19 @@
     </div>
 
     @include('frontend.include.js')
-
-
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/646dbf6374285f0ec46d5062/1h16b77gk';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
