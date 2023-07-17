a28ce11863d027161ff5ac8427565bf2473990bd
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-07-17 11:42:52 +0600

    Update 5.5.9 loading

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'RBPBD ',
+    'title' => 'PRB BD ',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>RBPBD </b>Admin',
+    'logo' => '<b>RBP BD </b>Admin',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
