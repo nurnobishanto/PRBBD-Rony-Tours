1e2dbbf8c28e93e9e3c059d36e10f12de5527042
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-07-21 10:04:28 +0600

    Add Meta Description

--- resources/views/frontend/include/css.blade.php
+++ resources/views/frontend/include/css.blade.php
@@ -1,18 +1,20 @@
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset(getSetting('site_favicon')) }}">
     <!-- Title -->
     <title>{{getSetting('site_title')}} - {{getSetting('site_tagline')}}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Explore an array of incredible travel deals and seamless booking experiences with PRBBD Online Travel Agency. Plan your perfect getaway and embark on unforgettable adventures worldwide.">
+    <meta name="keywords" content="Travel agency, online travel booking, vacation deals, dream destinations, seamless booking experiences, worldwide adventures.">
 
     <!-- Bootstrap css -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
     <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-{{--    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>--}}
     <!-- animate css -->
     <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}" />
     <!-- Fontawesome css -->
     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.all.min.css') }}" />
-    {{-- <link rel="stylesheet" href="../../../../../cdn.jsdelivr.net/npm/bootstrap-icons%401.8.2/font/bootstrap-icons.css"> --}}
     <!-- owl.carousel css -->
     <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}" />
     <!-- owl.theme.default css -->
@@ -26,9 +28,7 @@
     <!-- Responsive css -->
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
     {{-- <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" /> --}}
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="{{ asset(getSetting('site_favicon')) }}">
-{{--    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/smoothness/jquery-ui.css">--}}
+
     <link rel="stylesheet" href="{{asset('vendor/jquery-ui/jquery-ui.css')}}">
 
     <script src="{{asset('vendor/jquery-ui/jquery-ui.min.js')}}"></script>
