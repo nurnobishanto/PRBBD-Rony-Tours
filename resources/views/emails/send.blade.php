538d21588eb54a9e7e9f3835d235ab20169dbd45
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-08-03 21:36:28 +0600

    Update registration and email

--- resources/views/emails/send.blade.php
+++ resources/views/emails/send.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PRB BD -  Online Travel Agency</title>
-    <style>
+    <title>PRB BD - Online Travel Agency</title>
+    <style type="text/css">
         /* Add your CSS styles here */
         /* For example: */
         body {
@@ -24,23 +24,29 @@
             font-size: 12px;
             color: #999;
         }
-        .content{
-            padding:50px;
+        .content {
+            padding: 50px;
+        }
+        div {
+            line-height: 20px;
         }
     </style>
 </head>
 <body>
 <div class="header">
     <img width="100px" src="{{getSetting('site_logo')}}">
-    <h1>PRB BD -  Online Travel Agency</h1>
+    <h1>PRB BD - Online Travel Agency</h1>
 </div>
 
 <div class="content">
     <p>{{ $dynamicData['body'] }}</p>
-    <br>
-    <br>
-    Regards.
-    PRB BD -  Online Travel Agency
+    <div>Regards,</div>
+    <div>PRB BD - Online Travel Agency</div>
+    <div>Bakra, High School Road Bakra, Jashore</div>
+    <div>Jashore, Dhaka, Bangladesh.</div>
+    <div>Hotline: <a href="tel:+881978-250612">+881978-250612</a> | <a href="tel:+8801978-250611">+8801978-250611</a> </div>
+    <div>Web: <a href="https://prbbd.com">https://prbbd.com</a> </div>
+    <p style="text-align: justify">The contents of this email and any attachments are confidential and may also be legally privileged. If you are not the intended recipient(s), any use of the information contained herein is prohibited and may be unlawful. In such an event, you are requested to delete the email and intimate the sender. We are not responsible for the accuracy or completeness of this email as it has been transmitted over a public network and may suffer from errors, viruses, delay, interception, and amendment. Whenever any agreement is being entered based on the content of this email, the content of the agreement duly approved by PRBBD Online Travel Agency shall be considered as binding and not that of the email.</p>
 </div>
 
 <div class="footer">
@@ -49,4 +55,3 @@
 </div>
 </body>
 </html>
-
