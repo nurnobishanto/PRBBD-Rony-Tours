4e8987eee5df17f37ca8595275ac265be4c3af79
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-08-09 12:50:12 +0600

    Bug Fix Forget password 1.0

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -29,12 +29,14 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::guard('web')->sendResetLink(
+        // Specify the guard to be used (in this case, 'web' guard)
+        $guard = 'web';
+
+        // Send the password reset link using the specified guard
+        $status = Password::broker($guard)->sendResetLink(
             $request->only('email')
         );
+
 
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
